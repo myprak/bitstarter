ed89b38e85a05ba5403d68b5ba1b22bc672de823
Prakash Narayanan <myprak@gmail.com>
2014-03-14 07:23:36 +0000

    Mod web.js to read in index.html

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var app = express();
 app.use(express.logger());
 
 
-//var inputbuf = new Buffer(fs.readFileSync('index.html'));
+var inputbuf = new Buffer(fs.readFileSync('index.html'));
 
 //var newoutput = inputbuf.toString('utf8');
+
 var newoutput = "hllow";
 
 app.get('/', function(request, response) {
