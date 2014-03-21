5ce93dd4d567e5dc232ceef420f3ecd79666df51
Ubuntu <ubuntu@ip-172-31-13-51.us-west-2.compute.internal>
2014-03-21 11:30:21 +0000

    Final homework 4 bitstarter

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
+
+var htmlfile = "index.html";
+
+var app = express ();
 app.use(express.logger());
 
-
-var inputbuf = new Buffer(fs.readFileSync('index.html'));
-var newoutput = inputbuf.toString('utf8');
-
 app.get('/', function(request, response) {
-  response.send(newoutput);
-
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
